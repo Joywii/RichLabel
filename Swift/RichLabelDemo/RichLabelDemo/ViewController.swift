0ff87ec28f9209f54682b72b929b034e2f7ac879
zhongweitao <weitaozhong@sohu-inc.com>
2014-12-10 14:43:36 +0800

    add swift RichLabelDemo

---
+++ Swift/RichLabelDemo/RichLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichLabelDemo
+//
+//  Created by joywii on 14/12/10.
+//  Copyright (c) 2014年 joywii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
